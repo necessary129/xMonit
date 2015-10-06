5eb2446c00bda8a98e6255e0629f1d2b69b26751
Shamil K Muhammed <mail+github@shamilk.me>
2015-10-06 23:26:33 +0530

    eh3

--- src/commands2.py
+++ src/commands2.py
@@ -201,7 +201,7 @@ def check(cli, rnick, chan, rest):
     snick = nick.replace(r"[",r"\[").replace(r"]",r"\]").replace("\\","\\\\").replace(r"{",r"\{").replace(r"}",r"\}")
     sident = user.replace(r"[",r"\[").replace(r"]",r"\]").replace("\\","\\\\").replace(r"{",r"\{").replace(r"}",r"\}")
     sip = cloak.replace(r"[",r"\[").replace(r"]",r"\]").replace("\\","\\\\").replace(r"{",r"\{").replace(r"}",r"\}")
-    p = Popen('cat raw.log | egrep \'snick|sident|sip\' | egrep \'!keep|!approve\'',shell=True)
+    p = Popen('cat raw.log | egrep \'snick|sident|sip\' | egrep \'!keep|!approve\'',shell=True,stdout=PIPE,stderr=STDOUT)
     t = p.communicate()
     if not t == "":
         ever = t
